ca7697c53ebf8284332c7ff148f018804b07c709
Naufal Suryanto <naufalsuryanto@gmail.com>
2023-08-17 10:12:53 +0900

    Add arxiv link, fix demo video scripts

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
-function simulation_video(action, video_name){
-    const attack_types = ["normal", "random", "camou", "er", "dta", "dta2"];
-    for (i in attack_types){
+function simulation_video(action, video_name) {
+    const attack_types = ["normal", "random", "naive", "camou", "er", "fca", "das", "dta", "active"];
+    for (i in attack_types) {
         let video_obj = document.getElementById(video_name + "_" + attack_types[i]);
-        if (action == "play"){
-            if (video_obj.paused) 
-                video_obj.play(); 
-            else 
-                video_obj.pause(); 
-        } else if (action == "restart"){
+        if (action == "play") {
+            if (video_obj.paused)
+                video_obj.play();
+            else
+                video_obj.pause();
+        } else if (action == "restart") {
             video_obj.load()
         }
     }
 }
 
 function transferability_video(action, video_name) {
-    const attack_types = ["normal", "upc", "das", "random", "camou", "er", "dta", "dta2"];
+    const attack_types = ["normal", "random", "naive", "camou", "er", "dta", "active"];
+    for (i in attack_types) {
+        let video_obj = document.getElementById(video_name + "_" + attack_types[i]);
+        if (action == "play") {
+            if (video_obj.paused)
+                video_obj.play();
+            else
+                video_obj.pause();
+        } else if (action == "restart") {
+            video_obj.load()
+        }
+    }
+}
+
+function segmentation_video(action, video_name) {
+    const attack_types = ["normal", "ACTIVE", "normal_SG", "ACTIVE_SG"];
     for (i in attack_types) {
         let video_obj = document.getElementById(video_name + "_" + attack_types[i]);
         if (action == "play") {
